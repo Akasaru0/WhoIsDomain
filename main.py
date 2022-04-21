c0253c0fb67d2dbffac71d4c08778722a696a43d
AkaSaru <akasaru0@protonmail.com>
2022-04-21 11:37:28 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import argparse
+from whoisapi import *
+import json
+
 
 parser = argparse.ArgumentParser(description='Get Information About a Domain Name')
 parser.add_argument('-d','--domain',help='The domain name to be checked',required=True)
@@ -6,17 +9,12 @@ parser.add_argument('-o','--ouput-file',help='Ouput File Name. If not specify th
 
 args = parser.parse_args()
 domain_name = args.domain
-
-from whoisapi import *
-import json
-
-client = Client(api_key='at_ws24xwuGq75oCQYMZXD7Egc1B3qcU',outputFormat='JSON')
-# Get parsed whois record as a model instance.
-print('[x] Query WhoisAPIXML for the domain :'+domain_name)
-whois = json.loads(client.raw_data(domain_name))
-print('[x] Query received')
-# Get particular field of the whois record
-print('[x] Query Information :')
-print('     ~> Creation Date :'+whois["WhoisRecord"]["createdDate"])
-print('     ~> Name of the Registrant : '+whois["WhoisRecord"]["registrant"]["name"])
-print('     ~> Organization : '+whois["WhoisRecord"]["administrativeContact"]["organization"])
+print(args.outputfile)
+#client = Client(api_key='at_ws24xwuGq75oCQYMZXD7Egc1B3qcU',outputFormat='JSON')
+#print('[x] Query WhoisAPIXML for the domain :'+domain_name)
+#whois = json.loads(client.raw_data(domain_name))
+#print('[x] Query received')
+#print('[x] Query Information :')
+#print('     ~> Creation Date :'+whois["WhoisRecord"]["createdDate"])
+#print('     ~> Name of the Registrant : '+whois["WhoisRecord"]["registrant"]["name"])
+#print('     ~> Organization : '+whois["WhoisRecord"]["administrativeContact"]["organization"])
