3edc10b12b7cf3613cd861be86c906e909701f4f
AkaSaru <akasaru0@protonmail.com>
2022-04-21 11:44:08 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import argparse
 from whoisapi import *
 import json
-
+import csv
 
 parser = argparse.ArgumentParser(description='Get Information About a Domain Name')
 parser.add_argument('-d','--domain',dest='domain',help='The domain name to be checked',required=True)
@@ -9,12 +9,15 @@ parser.add_argument('-o','--output-file',dest="output",help='Output File Name. I
 
 args = parser.parse_args()
 domain_name = args.domain
-print(args.output)
-#client = Client(api_key='at_ws24xwuGq75oCQYMZXD7Egc1B3qcU',outputFormat='JSON')
-#print('[x] Query WhoisAPIXML for the domain :'+domain_name)
-#whois = json.loads(client.raw_data(domain_name))
-#print('[x] Query received')
-#print('[x] Query Information :')
-#print('     ~> Creation Date :'+whois["WhoisRecord"]["createdDate"])
-#print('     ~> Name of the Registrant : '+whois["WhoisRecord"]["registrant"]["name"])
-#print('     ~> Organization : '+whois["WhoisRecord"]["administrativeContact"]["organization"])
+client = Client(api_key='at_ws24xwuGq75oCQYMZXD7Egc1B3qcU',outputFormat='JSON')
+print('[x] Query WhoisAPIXML for the domain :'+domain_name)
+whois = json.loads(client.raw_data(domain_name))
+print('[x] Query received')
+print('[x] Query Information :')
+print('     ~> Creation Date :'+whois["WhoisRecord"]["createdDate"])
+print('     ~> Name of the Registrant : '+whois["WhoisRecord"]["registrant"]["name"])
+print('     ~> Organization : '+whois["WhoisRecord"]["administrativeContact"]["organization"])
+if args.output != "None":
+    with open(args.output+'.csv',newline=''):
+        writer = csv.writer(args.output+'.csv',delimiter='/')
+        writer.writerow(whois["WhoisRecord"]["createdDate"],whois["WhoisRecord"]["registrant"]["name"],whois["WhoisRecord"]["administrativeContact"]["organization"])
